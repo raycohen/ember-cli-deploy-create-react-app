8f98b50a4f19d06c3597b738b240abd2eb456105
Ray Cohen <rcohen@paperlesspost.com>
2016-12-01 16:25:27 -0500

    ember deploy production builds the create react app and outputs the files in the build folder

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 /* jshint node: true */
 'use strict';
 
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+var Promise = require('ember-cli/lib/ext/promise');
+var exec = require('child_process').exec;
+var glob = require('glob');
+
 module.exports = {
-  name: 'ember-cli-deploy-create-react-app'
+  name: 'ember-cli-deploy-create-react-app',
+
+  createDeployPlugin: function(options) {
+    var Plugin = DeployPluginBase.extend({
+      name: options.name,
+
+      defaultConfig: {},
+
+      build: function(context) {
+        var self = this;
+        var options = {};
+        options.cwd = '../fluxproofofconcept';
+
+        return new Promise(function(resolve, reject) {
+          exec('npm run build', options, function(error, stdout, stderr) {
+            if (error) {
+              reject();
+              return;
+            }
+
+            var files = glob.sync('**/**/*', {cwd: '../fluxproofofconcept/build'});
+
+            if (files && files.length) {
+              files.forEach(function(path) {
+                self.log('✔  ' + path, { verbose: true });
+              });
+            } else {
+              self.log('No files :(');
+            }
+
+            resolve({
+              distDir: '../fluxproofofconcept/build',
+              distFiles: files
+            });
+          });
+        }).then(function(results) {
+          exec('ls', options, function(error, stdout, stderr) {
+            if (error) {
+              reject();
+              return;
+            }
+            resolve(results);
+          });
+        });
+      }
+    });
+
+    return new Plugin();
+  }
 };
