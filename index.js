0963dd76cd9e7462e2abf17009c12b5b0301e9ac
Raymond Cohen <raycohen@gmail.com>
2017-02-13 23:16:46 -0500

    warn if ember-cli-deploy-build is in use and not disabled
    if a user is using a plugin pack that specifies ember-cli-deploy-build
    as one of the plugins, deployment will error at the build phase because
    ember-cli-deploy-build will try to build an ember project, and error
    when it doesn't find ember-cli-build.js
    
    ember-cli-deploy allows a user to disable specific plugins from the ENV object
    exported in config/deploy.js
    
    To do so, export an ENV such that `ENV.pipeline.disabled.build === true`

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ var DeployPluginBase = require('ember-cli-deploy-plugin');
 var Promise = require('ember-cli/lib/ext/promise');
 var exec = require('child_process').exec;
 var glob = require('glob');
+var nodeModulesPath = require('node-modules-path');
+var path = require('path');
+var fs = require('fs');
+
+var getDisabledPluginsMap = function(context) {
+  if (context && context.config && context.config.pipeline) {
+    return context.config.pipeline.disabled;
+  }
+  return {};
+};
+
+var userHasDisabledBuildPlugin = function(context) {
+  return getDisabledPluginsMap(context).build;
+};
 
 module.exports = {
   name: 'ember-cli-deploy-create-react-app',
@@ -16,6 +30,38 @@ module.exports = {
       requiredConfig: ['publicURL'],
 
       defaultConfig: {},
+
+      configure: function(context) {
+        this.warnIfBuildPluginNotDisabled(context);
+      },
+
+      warnIfBuildPluginNotDisabled(context) {
+        if (userHasDisabledBuildPlugin(context)) {
+          return;
+        }
+
+        var modulesPath = nodeModulesPath(process.cwd());
+        var buildPluginPath = path.join(modulesPath, 'ember-cli-deploy-build');
+        if (fs.existsSync(buildPluginPath)) {
+          this.warn('The ember build plugin must be disabled.');
+          this.warn('Add the following to your config/deploy.js file:');
+          this.warn('```', {color: 'red'});
+          this.logCode('ENV.pipeline = {');
+          this.logCode('  disabled: {');
+          this.logCode('    build: true');
+          this.logCode('  }');
+          this.logCode('};');
+          this.warn('```', {color: 'red'});
+        }
+      },
+
+      warn(message) {
+        this.log(message, {color: 'red'});
+      },
+
+      logCode(message) {
+        this.log(message, {color: 'green'});
+      },
 
       build: function(context) {
         var self = this;
